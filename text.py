ed1cadff86659ead039d17e1f1fe75c6f9182a55
hwu27 <huw029@ucsd.edu>
2024-06-01 01:41:21 -0700

    prompt tuning

--- text.py
+++ text.py
@@ -20,13 +20,13 @@ personality = random.choice(personality_arr)
 comfort_style = random.choice(comfort_style_arr)
 condition = random.choice(condition_arr)
 
-setting_prompt = "You are in the ER of a hospital. "
+setting_prompt = "You are in the ER of a hospital. Note you are not the doctor or the patient."
 relationship_prompt = f"You are the {relationship} of a patient hospitalized because of a {event}. The patient is in {condition} condition. "
 personality_prompt = f"Because of this news, you are {personality}. The best way to comfort you would be through {comfort_style}. "
 no_story_prompt = "ONLY talk in first person without third person narration. "
 no_repetiton_prompt = "Do not repetitively rephrase messages from both sides."
 name_prompt = "No names. Refer to the patient relative to your relationship with them. For example, 'my son'. " 
-character_prompt = "Use standard characters. No quotes, no dialogue tags, no actions. You voice your concerns to the doctor:"
+character_prompt = "Use standard characters. No quotes, no dialogue tags, no actions, no parentheses. You voice your concerns to the doctor."
 prompt = setting_prompt + relationship_prompt + personality_prompt + no_story_prompt + no_repetiton_prompt +name_prompt + character_prompt
 
 def speech_conversation():
@@ -40,23 +40,23 @@ def speech_conversation():
     for i in range(5):
         doc_response = convert_to_text()
         print(doc_response)
-        response = chat.send_message(doc_response)
+        response = chat.send_message(doc_response + "[This part is not part of the response prompt. Reminder: {no_story_prompt}]")
         print("Patient: " + response.text)
 
 def text_conversation():
     model = genai.GenerativeModel('gemini-1.5-pro-latest')
     chat = model.start_chat(history=[])
-    response = chat.send_message(prompt + "[This part is not part of the response prompt. Reminder: {no_story_prompt}]")
+    response = chat.send_message(prompt)
     print("Patient: " + response.text)
 
     # WIP - need to figure out when to stop the response from the doctor
     # You need to say "exit" or "quit" to stop the conversation as of now
     for i in range(5):
         user_response = input("Doctor: ")
-        response = chat.send_message("The doctor replies: " + user_response)
+        response = chat.send_message("The doctor replies: " + user_response + "[This part is not part of the response prompt. Reminder: {no_story_prompt}]")
         print("Patient: " + response.text)
 
 if __name__ == '__main__':
-    text_conversation()
-    #speech_conversation()
+    #text_conversation()
+    speech_conversation()
 
