af24a60759b390e3ed2095bb40dc996fcf0db9c5
hwu27 <huw029@ucsd.edu>
2024-06-03 12:18:09 -0700

    added react + firebase implementation

---
+++ frontend/app/page.jsx
@@ -0,0 +1,10 @@
+'use client'
+import React, { useState, useEffect, useRef } from 'react';
+import Login from './pages/Login'
+export default function page() {
+  return (
+    <>
+      <Login />
+    </>
+  );
+}
